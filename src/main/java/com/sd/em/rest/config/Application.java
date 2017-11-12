b70427cb045a92a841186833a1c7a0cac980f68c
sapandesai11 <sapandesai11@gmail.com>
2017-11-12 19:11:55 +0530

    Added Application class

---
+++ src/main/java/com/sd/em/rest/config/Application.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.sd.em.rest.config;
+
+import org.springframework.boot.SpringApplication;
+
+
+/**
+ * @author PC Doctor.com
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Application.class, args);
+	}
+
+}
